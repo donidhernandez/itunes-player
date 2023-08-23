23bfe47939061b08c56b965381136cef0b85e1eb
Adonai Dominguez <adonaidominguezes@gmail.com>
2023-08-23 19:01:28 +0200

    feat(initial-setup): add Eslint, Prettier, Husky, Commitizen and Tailwind
    Create a full setup to start working on the current project

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,9 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <main className="h-screen bg-slate-900">
+            <h1 className="text-white text-6xl">Itunes App</h1>
+        </main>
+    )
 }
 
-export default App;
+export default App
