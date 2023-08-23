da687e51da15a356c0cc05773d165a5ed0ed4fee
Adonai Dominguez <adonaidominguezes@gmail.com>
2023-08-23 20:00:04 +0200

    feat(searchbar): create the initial search-bar ui component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import SearchBar from './components/SearchBar'
+
 function App() {
     return (
         <main className="h-screen bg-slate-900">
             <h1 className="text-white text-6xl">Itunes App</h1>
+            <SearchBar />
         </main>
     )
 }
